3a2acef9e6d62b1623ec95da2260bb5e4b51aa19
AnkurDesai11 <60006478+AnkurDesai11@users.noreply.github.com>
2022-05-04 01:11:19 +0530

    added dynamic navbar

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const { route } = require("express/lib/application");
-
+const Details = require("../models/Details")
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    //console.log("in get /");
-    res.render("index")
+    const detail = await Details.findOne()
+    //console.log(detail)
+    res.render("index", { details: detail })
 });
 
 router.get("/gallery", async (req, res) => {
-    //console.log("in get /");
-    res.render("gallery")
+    const detail = await Details.findOne()
+    res.render("gallery", { details: detail })
 });
 
 module.exports = router;
